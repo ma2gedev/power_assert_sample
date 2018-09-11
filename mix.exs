e99bc82ab497917e9db252e2ca1216a524e60e8a
Takayuki Matsubara <takayuki.1229@gmail.com>
2018-09-11 20:05:35 +0900

    update power_assert version

--- mix.exs
+++ mix.exs
@@ -27,6 +27,6 @@ defmodule PowerAssertSample.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:power_assert, "~> 0.0.3"}]
+    [{:power_assert, "~> 0.1.0"}]
   end
 end
